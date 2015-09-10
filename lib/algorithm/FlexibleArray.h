0693fa52eb8fd28c27954cc4ce04c3281bdc576b
ruleless <ruleless@126.com>
2015-09-11 04:22:02 +0800

    单元测试目录整理；简单散列表实现

--- lib/algorithm/FlexibleArray.h
+++ lib/algorithm/FlexibleArray.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "common/common.h"
 #include "math.h"
 
 template <typename T>
@@ -49,6 +50,64 @@ class FlexibleArray
 		this->log2size_ = 0;
 	}
 
+	const T* get(int key)
+	{
+		const Node *n = this->getNode(key);
+		if (n)
+			return &n->i_val;
+		else
+			return NULL;
+	}
+
+	Node* getNode(int key)
+	{
+		if (key < 0)
+			return NULL;
+
+		int sz = 1 << this->log2size_;
+		Node *n = &this->node_[key & (sz-1)]
+		for (;;)
+		{
+			if (n->i_key.nk.n == key)
+				return n;
+			if (0 == n->i_key.nk.next)
+				break;
+			n += n->i_key.nk.next;
+		}
+		return NULL;
+	}
+
+	T* set(int key, const T & val)
+	{
+		Node *n = this->getNode(key);
+		if (n)
+		{
+			n->i_val = val;
+			return &n->i_val;
+		}
+
+		int sz = 1 << this->log2size_;
+		n = &this->node_[key & (sz-1)];
+		if (!this->_isNil(n) && n->i_key.n != key)
+		{
+			Node *f = this->_getLastFree();
+			if (NULL == f)
+			{
+				this->_resize(sz+1);
+				f = this->_getLastFree();
+			}
+			Assert(f);
+
+			if (n->i_key.nk.next != 0)
+				f->i_key.nk.next = (n + n->i_key.nk.next) - f;
+			n->i_key.nk.next = f-n;
+
+			n = f;
+		}
+		n->i_val = val;
+		return &n->i_val;
+	}
+
 	void _setNil(Node *n)
 	{
 		(n->i_key).nk.n = -1;
@@ -61,14 +120,40 @@ class FlexibleArray
 	}
 
 	void _resize(int newsz)
-	{
-		this->log2size_ = ceillog2(newsz);
+	{		
+		int log2sz = ceillog2(newsz);
+		Assert(log2sz > this->log2size_);		
+				
+		int newsz = 1 << log2sz;
+		int newVec = new Node[newsz];
 		int sz = 1 << this->log2size_;
-		int newVec = new Node[sz];
-		for (int i = 0; i < sz; ++i)
+		for (int i = 0; i < newsz; ++i)
 		{
-			if ()
+			if (i < sz && this->node_[i].i_key.n >= 0)
+			{
+				newVec[i] = this->node_[i];
+			}
+			else
+			{
+				this->_setNil(&newVec[i]);
+			}
 		}
+
+		delete []this->node_;
+		this->log2size_ = log2sz;
+		this->node_ = newVec;
+		this->lastfree_ = newVec+newsz-1;
+	}
+
+	Node* _getLastFree()
+	{
+		while (this->lastfree_ > this->node_)
+		{
+			--this->lastfree_;
+			if (this->_isNil(this->lastfree_))
+				return this->lastfree_;
+		}
+		return NULL;
 	}
   private:
 	Node *node_;

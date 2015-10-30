aaa05232dd5728070823bd0bd04adf97d8021ec6
ruleless <ruleless@126.com>
2015-10-30 18:27:55 +0800

    snail回射测试程序Bug修复

--- UnitTest/NetTest/EchoClient.cpp
+++ UnitTest/NetTest/EchoClient.cpp
@@ -1,8 +1,11 @@
 #include "EchoMessage.h"
 #include "network/Channel.h"
 #include "network/Bundle.h"
+#include "common/MemoryStream.h"
 
 void EchoMessage::handle(Channel *pChannel, MemoryStream &s)
 {
-	printf("serv():%s\n", s.data());
+	std::string str;
+	s>>str;
+	printf("serv():len:%lu data:%s\n", str.length(), (char *)str.c_str());
 }
--- UnitTest/NetTest/EchoServer.cpp
+++ UnitTest/NetTest/EchoServer.cpp
@@ -4,26 +4,17 @@
 
 void EchoMessage::handle(Channel *pChannel, MemoryStream &s)
 {
-	char buf[1024];
 	std::string fbuffer;
+	s>>fbuffer;
 
-	__snprintf(buf, 1024, "wpos: %lu, rpos=%lu.\n", (unsigned long)s.wpos(), (unsigned long)s.rpos());
-	fbuffer += buf;
-
-	for(uint32 i = s.rpos(); i < s.wpos(); ++i)
-	{
-		__snprintf(buf, 1024, "%c", s.read<uint8>(i));
-		fbuffer += buf;
-	}
-
-	printf("cli(%s):%s\n", pChannel->addr().ipAsString(), fbuffer.c_str());
+	printf("cli(%s):len:%d %s", pChannel->addr().ipAsString(), fbuffer.length(), fbuffer.c_str());
 	// printf("cli(%s):%s len:%d\n", pChannel->addr().ipAsString(), s.data(), s.length());
 
 	Bundle *pBundle = Bundle::ObjPool().createObject();
 	if (pBundle)
 	{
 		pBundle->newMessage(*this);
-		pBundle->append(s);
+		(*pBundle)<<fbuffer;
 
 		pChannel->send(pBundle);
 	}

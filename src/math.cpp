b207fd8f1cc59e6c0680cbf90d7280ed18f33832
ruleless <ruleless@126.com>
2016-01-17 17:41:48 +0800

    修复vs2005下的一处编译报错

--- src/math.cpp
+++ src/math.cpp
@@ -6,12 +6,20 @@ namespace math
 {
 void srandom(int seed)
 {
+#if PLATFORM == PLATFORM_WIN32
+	srand(seed);
+#else
 	::srandom(seed);
+#endif
 }
 
 int random(int low, int high)
 {
+#if PLATFORM == PLATFORM_WIN32
+	double r = (double)::rand() * (1.0 / ((double)RAND_MAX + 1.0));
+#else
 	double r = (double)::random() * (1.0 / ((double)RAND_MAX + 1.0));
+#endif
 	r *= (double)(high - low) + 1.0;
 	return (int)r + low;
 }
